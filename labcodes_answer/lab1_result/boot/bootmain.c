accc951c70487b749343af406c9d165d4fb4f14f
wbaiting <771050765@qq.com>
2018-07-04 17:24:24 +0800

    添加实验一的部分注释，已完成boot文件夹的注释

--- labcodes_answer/lab1_result/boot/bootmain.c
+++ labcodes_answer/lab1_result/boot/bootmain.c
@@ -29,8 +29,10 @@
  *
  *  * bootmain() in this file takes over, reads in the kernel and jumps to it.
  * */
+/*此文件目的是在bootasm.s切换到保护模式并设置堆栈之后，将内核读取到相应的位置，然后跳转到内核运行*/
 unsigned int    SECTSIZE  =      512 ;
 struct elfhdr * ELFHDR    =      ((struct elfhdr *)0x10000) ;     // scratch space
+//指定ELFHDR的存放位置，头部将被读取到这个内存地址处，经测试可以修改为其他可用的内存地址
 
 /* waitdisk - wait for disk ready */
 static void
@@ -63,11 +65,20 @@ readsect(void *dst, uint32_t secno) {
  * readseg - read @count bytes at @offset from kernel into virtual address @va,
  * might copy more than asked.
  * */
+/*
+ * 功能描述：
+ *     读取磁盘中从第二个扇区算起偏移量为offset的位置开始往后的count个字节
+ *     到虚拟地址va中，
+ */
 static void
 readseg(uintptr_t va, uint32_t count, uint32_t offset) {
     uintptr_t end_va = va + count;
 
     // round down to sector boundary
+	// 因为readsect读取是按扇区读取，所以先把offset对齐为sectsize，
+	// 例如offset为513，我们可以知道要从第二个扇区的第二个字节读取，
+	// 但是readsect按扇区为单位读取，因此还是会把偏移为512的那个字节读取，
+	// 现在为了让第513字节处于va处，于是把va减去1.
     va -= offset % SECTSIZE;
 
     // translate from bytes to sectors; kernel starts at sector 1
@@ -85,6 +96,7 @@ readseg(uintptr_t va, uint32_t count, uint32_t offset) {
 void
 bootmain(void) {
     // read the 1st page off disk
+	// 为什么读取一页，留下疑问，^puzzle1^
     readseg((uintptr_t)ELFHDR, SECTSIZE * 8, 0);
 
     // is this a valid ELF?
@@ -95,6 +107,9 @@ bootmain(void) {
     struct proghdr *ph, *eph;
 
     // load each program segment (ignores ph flags)
+	// ph程序段的起始地址，eph程序段的终止地址
+	// 与0xFFFFFF与操作是为了将地址映射到0-4M,因为在这之前
+	// 地址空间是直接线性地址=物理地址，因此需要将逻辑地址变成线性地址
     ph = (struct proghdr *)((uintptr_t)ELFHDR + ELFHDR->e_phoff);
     eph = ph + ELFHDR->e_phnum;
     for (; ph < eph; ph ++) {
